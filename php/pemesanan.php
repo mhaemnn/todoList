b3f567fc4df585d2f4d7fd01bbf59f55ad2c0444
mhaemnn <ismuhaimin1@gmail.com>
2022-10-28 09:03:17 +0700

    modifikasi

--- php/pemesanan.php
+++ php/pemesanan.php
@@ -14,34 +14,46 @@
 <body>
   <div class="container">
     <h2>From Pemesanan</h2>
-    <form action="/action_page.php" method="post">
-      <table cellspacing="20" cellpadding="0">
+    <table cellspacing="20" cellpadding="0">
+      <form action="/action_page.php" method="post">
+
+        <!-- namapengguna -->
         <tr>
           <td><label for="nama">Nama Lengkap</label></td>
           <td colspan="2"><input type="text" id="nama"></td>
         </tr>
+
+        <!-- no indentitas -->
         <tr>
           <td><label for="identitas">Nomor Identitas</label></td>
           <td colspan="2"><input type="number" id="identitas"></td>
         </tr>
+
+        <!-- No hp -->
         <tr>
           <td><label for="phone">No. HP</label></td>
           <td colspan="2"><input type="tel" id="phone" placeholder="+62"></td>
         </tr>
+
+        <!-- kelas penumpang -->
         <tr>
           <td><label for="penumpang">Kelas Penumpang</label></td>
           <td colspan="2">
-            <select name="penumpang" id="penumpang" style="width:100% ;">
+            <select name="penumpang" id="penumpang" style="width:77% ;">
               <option value="">Eksekutif</option>
               <option value="">Bisnis</option>
               <option value="">Ekonimi</option>
             </select>
           </td>
         </tr>
+
+        <!-- jadwal keberangkatna -->
         <tr>
           <td><label for="jadwal_keber">Jadwal Keberangkatan</label></td>
-          <td colspan="2"><input type="date" id="jadwal_keber" style="width:96%" ;></td>
+          <td colspan="2"><input type="date" id="jadwal_keber" style="width: 15em" ;></td>
         </tr>
+
+        <!-- jumlah penumpang -->
         <tr>
           <td><label for="jmlPenumpang">Jumlah Penumpang</label></td>
           <td colspan="2"><input type="number" id="jmlPenumpang"></td>
@@ -50,6 +62,8 @@
           <td><label for="jmlPenumLansi">Jumlah Penumpang Lansia</label></td>
           <td colspan="2"><input type="number" id="jmlPenumLansi"></td>
         </tr>
+
+        <!-- harga tiket & bayar tiket -->
         <tr>
           <td><label for="setHarga">Harga Tiket</label></td>
           <td><label id="setHarga">0</label></td>
@@ -58,12 +72,20 @@
           <td><label for="setBayar">Total Bayar</label></td>
           <td><label id="setBayar">0</label></td>
         </tr>
-        <!-- <tr>
-          <td colspan="2">
-            <input type="submit" value="Hitung Total Harga"><input type="submit" value="Pesan Tiket"><input type="reset" value="Reset" onclick="msg()">
-          </td>
-        </tr> -->
-      </table>
+
+        <!-- checkbox menyutujui -->
+        <tr>
+          <td><input type="checkbox" class="ukuran-checkbox"></td>
+          <td><i>Saya dan atau rombongan telah membaca, memahami, dan setuju berdasarkan syarat dan ketentuan yang telah diterapkan</i></td>
+        </tr>
+
+        <!-- button pesan tiket, totol & reset -->
+        <tr>
+          <td></td>
+          <td><input type="submit" value="Pesan Tiket"> <input type="reset" value="Reset" onclick="msg()"></td>
+        </tr>
+
+    </table>
     </form>
   </div>
 </body>
